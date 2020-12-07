3577dacefd95e5190c76e7f0a8e2a53919dd0909
Guilherme <guilhermemolina00@gmail.com>
2020-12-07 01:43:33 -0300

    Update day1

--- day1/app.js
+++ day1/app.js
@@ -1,24 +1,22 @@
 var fs = require('fs')
 var content = fs.readFileSync('numbers.txt','utf8')
 
-var arr = []
 var lines = content.split('\n')
 
 for(var i=0; i<lines.length; i++){
-    arr.push(lines[i])
     for(var j=0; j<lines.length; j++){
-        if(parseInt(arr[i])+parseInt(arr[j])==2020){
-            var x0 = arr[i]
-            var y0 = arr[j]
+        if(parseInt(lines[i])+parseInt(lines[j])==2020){
+            var x0 = lines[i]
+            var y0 = lines[j]
         }
         for(var k=0; k<lines.length; k++){
-            if(parseInt(arr[i])+parseInt(arr[j])+parseInt(arr[k])==2020){
-                var x1 = arr[i]
-                var y1 = arr[j]
-                var z1 = arr[k]
+            if(parseInt(lines[i])+parseInt(lines[j])+parseInt(lines[k])==2020){
+                var x1 = lines[i]
+                var y1 = lines[j]
+                var z1 = lines[k]
             }
         }
     } 
 }
 
-console.log(`Part One ===== \n Number 1: ${x0} \n Number 2: ${y0} \n Puzzle's Answer = ${x0*y0} \n \n Part Two ===== \n Number 1: ${x1} \n Number 2: ${y1} \n Number 3: ${z1} \n Puzzle's Answer = ${x1*y1*z1}`)
+console.log(`Part One ===== \nNumber 1: ${x0} \nNumber 2: ${y0} \nPuzzle's Answer = ${x0*y0} \n \nPart Two ===== \nNumber 1: ${x1} \nNumber 2: ${y1} \nNumber 3: ${z1} \nPuzzle's Answer = ${x1*y1*z1}`)
